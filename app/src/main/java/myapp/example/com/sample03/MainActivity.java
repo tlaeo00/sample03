00caba02ebfdbcffad524ff9da287c651a854a46
심대진 <tlaeo00@gmail.com>
2018-12-04 14:58:41 +0900

    테스트 했던거 올림돠~

--- app/src/main/java/myapp/example/com/sample03/MainActivity.java
+++ app/src/main/java/myapp/example/com/sample03/MainActivity.java
@@ -1,13 +1,32 @@
 package myapp.example.com.sample03;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // down btn
+        findViewById(R.id.downBtn).setOnClickListener(view -> {
+            findViewById(R.id.upImg).setVisibility(View.INVISIBLE);
+            findViewById(R.id.downImg).setVisibility(View.VISIBLE);
+            Toast.makeText(getApplicationContext(), "down 눌렸어~~", Toast.LENGTH_LONG).show();
+        });
+
+        // up btn
+        findViewById(R.id.upBtn).setOnClickListener(view -> {
+            findViewById(R.id.upImg).setVisibility(View.VISIBLE);
+            findViewById(R.id.downImg).setVisibility(View.INVISIBLE);
+            Toast.makeText(getApplicationContext(), "up 눌렸어~~", Toast.LENGTH_LONG).show();
+        });
+
     }
+
 }
